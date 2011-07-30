32856eb35f83752a238dbd7b0a8ceb8f9f0abfb2
ponty <titi@pince.(none)>
2011-07-30 22:41:18 +0200

    fix more pde

--- tests/test_arduino.py
+++ tests/test_arduino.py
@@ -1,5 +1,6 @@
 from nose.tools import eq_
 from pyavrutils.arduino import Arduino, ArduinoCompileError
+from pyavrutils.util import tmpdir
 from unittest import TestCase
 
 
@@ -24,3 +25,53 @@ class Test(TestCase):
             except ArduinoCompileError:
                 print '    compile error:', cc.error_text.splitlines()
                 raise ArduinoCompileError
+    
+    def test_bad_dir(self):
+        d = tmpdir() / 'Bad'
+        d.makedirs()
+        f = d / 'Foo.pde'
+        f.write_text(Arduino.minprog)
+        Arduino().build(f)
+        
+    def test_file(self):
+        d = tmpdir() / 'Foo'
+        d.makedirs()
+        f = d / 'Foo.pde'
+        f.write_text(Arduino.minprog)
+        Arduino().build(f)
+
+    def test_more_files(self):
+        d = tmpdir() / 'Foo'
+        d.makedirs()
+        f = d / 'Foo.pde'
+        f.write_text('#include "x.h" \n int x=foo; \n' + Arduino.minprog)
+        (d / 'x.h').write_text('#define foo 3')
+        Arduino().build(f)
+
+    def test_more_pde(self):
+        d = tmpdir() / 'Foo'
+        d.makedirs()
+        f = d / 'Foo.pde'
+        f.write_text('''
+#include "x.h" 
+int x=foo; 
+''' + Arduino.minprog)
+        (d / 'x.h').write_text('''
+#define foo 3
+
+class X
+{
+void fun();
+};
+        ''')
+        (d / 'x.pde').write_text('''
+//#include "x.h" 
+
+void X::fun()
+{
+    int x=foo;
+}
+        ''')
+        Arduino().build(f)
+
+        
